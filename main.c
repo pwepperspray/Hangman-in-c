bf1b2f96b9acfc7d97f8b5afcbae85f410ec546a
cinnamon_rowll <166984309+pwepperspray@users.noreply.github.com>
2025-08-31 21:30:07 +0530

    Added a hangman display function

--- main.c
+++ main.c
@@ -1,33 +1,121 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-void run(FILE *file){
-	
-	int count = 0, r = rand() % 213;
-	char word[15];
+#define MAX_CHANCES 7
+
+void word_selector(char* word);
+void display(int state);
+
+int main(){
+	srand(time(NULL));
+	char word[15]; //the size 15 has been selected arbitrarily	
+	for(int i = 0; i < 8; i++){
+		display(i);
+		printf("\n");
+	}	    
+	return 0;
+}
+
+void word_selector(char* word){
+	int count = 0, randomNumber = rand() % 213;
+	FILE *file;
+
+	file = fopen("wordbank","r");
+	if(file == NULL){
+		printf("Cannot open file \n");
+		exit(2);
+	}
 	while(fgets(word,14,file) != 0){
 		count++;
-		if(count == r){
+		if(count == randomNumber){
 			break;
 		}
 	}
-	printf("count : %d, Word : %s \n", count, word);
-
+	fclose(file);
 }
 
-int main(){
-	FILE *file;
+/* "  +---+"
+"  |   |",
+"  O   |",
+" /|\  |",
+" / \  |",
+"      |",
+"========="}; */
+
+void display(int state){
+
+	printf("+++++++++++++HANGMAN+++++++++++++\n");
+	const char *row0[] = {"  +---+","      |","========="},
+			   *row1[] = {"  |   |"},
+			   *row2[] = {"  O   |"},
+			   *row3[] = {"  |   |"," /|   |"," /|\\  |"},
+			   *row4[] = {" /    |"," / \\  |"};
 	
-	file = fopen("wordbank","r");
-	if(file == NULL){
-		puts("Cannot open file");
-		exit(2);
+	if(state == 0){
+		printf("%s\n",row0[0]);
+		printf("%s\n",row0[1]);
+		printf("%s\n",row0[1]);
+		printf("%s\n",row0[1]);
+		printf("%s\n",row0[1]);
+		printf("%s\n",row0[1]);
+		printf("%s\n",row0[2]);
+	}else if(state == 1){
+		printf("%s\n",row0[0]);
+		printf("%s\n",row1[0]);
+		printf("%s\n",row0[1]);
+		printf("%s\n",row0[1]);
+		printf("%s\n",row0[1]);
+		printf("%s\n",row0[1]);
+		printf("%s\n",row0[2]);
+	}else if(state == 2){
+		printf("%s\n",row0[0]);
+		printf("%s\n",row1[0]);
+		printf("%s\n",row2[0]);
+		printf("%s\n",row0[1]);
+		printf("%s\n",row0[1]);
+		printf("%s\n",row0[1]);
+		printf("%s\n",row0[2]);
+	}else if(state == 3){
+		printf("%s\n",row0[0]);
+		printf("%s\n",row1[0]);
+		printf("%s\n",row2[0]);
+		printf("%s\n",row3[0]);
+		printf("%s\n",row0[1]);
+		printf("%s\n",row0[1]);
+		printf("%s\n",row0[2]);
+	}else if(state == 4){
+		printf("%s\n",row0[0]);
+		printf("%s\n",row1[0]);
+		printf("%s\n",row2[0]);
+		printf("%s\n",row3[1]);
+		printf("%s\n",row0[1]);
+		printf("%s\n",row0[1]);
+		printf("%s\n",row0[2]);
+	}else if(state == 5){
+		printf("%s\n",row0[0]);
+		printf("%s\n",row1[0]);
+		printf("%s\n",row2[0]);
+		printf("%s\n",row3[2]);
+		printf("%s\n",row0[1]);
+		printf("%s\n",row0[1]);
+		printf("%s\n",row0[2]);
+	}else if(state == 6){
+		printf("%s\n",row0[0]);
+		printf("%s\n",row1[0]);
+		printf("%s\n",row2[0]);
+		printf("%s\n",row3[2]);
+		printf("%s\n",row4[0]);
+		printf("%s\n",row0[1]);
+		printf("%s\n",row0[2]);
+	}else if(state == 7){
+		printf("%s\n",row0[0]);
+		printf("%s\n",row1[0]);
+		printf("%s\n",row2[0]);
+		printf("%s\n",row3[2]);
+		printf("%s\n",row4[1]);
+		printf("%s\n",row0[1]);
+		printf("%s\n",row0[2]);
 	}
-
-	run(file);
-	run(file);
-	run(file);
-
-	return 0;
 }
